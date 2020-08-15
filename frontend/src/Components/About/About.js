af74b9026776ff06d2bbeee7961c9a75f9f825eb
manuortelli <manuelortelli1@gmail.com>
2020-08-15 15:26:47 -0300

    Cuarto

--- frontend/src/Components/About/About.js
+++ frontend/src/Components/About/About.js
@@ -1,12 +1,31 @@
-import React, { useState, userEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './About/About.css';
+import './About.css';
 
-About () => {
+function About () {
     const [information, setInformation] = useState({});
-    userEffect{() => (
-        axios.get(https://manuortelliportfoliobackend.herokuapp.com/about)
-    )}
-    return();
-};
+    useEffect(() => {
+        axios.get('https://manuortelliportfoliobackend.herokuapp.com/about')
+        .then(
+            res => setInformation(res.data[0])
+        )}, [setInformation])
+    return(
+        <div className="About">
+            <div className="Container-photo">
+                <img className="photo" src={information.photo}>
 
+                </img>
+            </div>
+            <div className="Description">
+                <h1></h1>
+                <h2></h2>
+                <p></p>
+                <ul className="Skills">
+
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default About;
