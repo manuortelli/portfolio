ad314093fc13b02d726e2702db9afec4f084fd1f
manuortelli <manuelortelli1@gmail.com>
2020-08-15 16:55:52 -0300

    some changes

--- frontend/src/Components/Portfolio/Portfolio.js
+++ frontend/src/Components/Portfolio/Portfolio.js
@@ -18,7 +18,7 @@ function Portfolio() {
                 {projects.length > 0 && projects.map((project, index) =>
                     <div className="individual-card" key={index}>
                         <h1>{project.name}</h1>
-                        <img className="img-project" alt="imagen-proyecto" src={project.image}></img>
+                        <img className="img-project" alt="imagen-proyecto" ></img>
                         <p>{project.descripcion}</p>
                         <a href={project.link}>Visitar proyecto</a>
                     </div>
